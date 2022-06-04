5fbd042a0530fd8d915356a3a87dc1fe8c7fa0be
gram15inch <leapthestairs@gmail.com>
2022-06-04 16:59:36 +0900

    6/4

--- app/src/main/java/com/nuhlp/nursehelper/ui/main/MainActivity.kt
+++ app/src/main/java/com/nuhlp/nursehelper/ui/main/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.nuhlp.nursehelper.ui.main
 
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.Menu
-import android.view.View
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -47,47 +42,54 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(_binding.root)
 
-        /* todo
-         앱바 만들기
-         앱바 액티비티와 연동
-         홈 프래그먼트 만들기*/
+
 
     }
 
-    private fun setBottomAppBar() =_binding.apply {
-        setSupportActionBar(appBarBottomDrawer.bottomAppBar)
-
-       appBarBottomDrawer.bottomAppBar.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-      /*  appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), mainLayout
-        )*/
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.navigation_drawer, menu)
+        menuInflater.inflate(R.menu.option_menu_drawer, menu)
         return true
     }
+
+
     override fun onSupportNavigateUp(): Boolean {
-        val navController =  _binding.appBarBottomDrawer.contentDrawer.mainNavHostFragment.findNavController()
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
+    // **** 초기화 메서드 *****
 
     private fun setIsLoginObserve(isLogin: LiveData<Boolean>){
         isLogin.observe(this) { _ ->
             choseActivity(_isLogin.value?:false)
         }
     }
+
+    private fun setBottomAppBar() =_binding.apply {
+        setSupportActionBar(appBarBottomDrawer.bottomAppBar)
+
+        appBarBottomDrawer.appBarFloatingActionButton.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        appBarBottomDrawer.bottomAppBar.setOnClickListener {
+            true
+        }
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+              setOf(
+                  R.id.item1, R.id.item2, R.id.item3
+              ), mainLayout
+          )
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    // **** 헬퍼 메서드 *****
 
     private fun startLoginActivity(){
         val intent = Intent(this, LoginActivity::class.java)
